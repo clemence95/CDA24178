c275795e214d5c49f0b9e6627a563186779d8483
clemence95 <boitelleclemence@outlook.fr>
2024-04-18 11:10:08 +0200

    .

--- Chevaux.sql
+++ Chevaux.sql
@@ -65,3 +65,46 @@ CREATE TABLE Suit(
    FOREIGN KEY(Numero_d_enregistrement__identifiant_) REFERENCES Cheval(Numero_d_enregistrement__identifiant_),
    FOREIGN KEY(Numero_de_veterinaire__Idantifiant_) REFERENCES Veterinaire(Numero_de_veterinaire__Idantifiant_)
 );
+
+| Tables               | Attributs                                | Type de données    | Contraintes                           |
+|----------------------|------------------------------------------|--------------------|---------------------------------------|
+| Cheval               | Numero_d_enregistrement__identifiant_    | Auto-incrément INT | PRIMARY KEY                          |
+|                      | Date_de_naissance                        | DATE               |                                     |
+|                      | ID_de_la_mere                            | VARCHAR(50)        |                                     |
+|                      | Race                                     | VARCHAR(50)        |                                     |
+|                      | Couleur                                  | VARCHAR(50)        |                                     |
+|                      | Sexe                                     | VARCHAR(50)        |                                     |
+|                      | Lieu_de_naissance                        | VARCHAR(50)        |                                     |
+
+| Proprietaire         | Numéro_de_proprietaire__identifiant_     | Auto-incrément INT | PRIMARY KEY                          |
+|                      | Nom                                      | VARCHAR(50)        |                                     |
+|                      | Prenom                                   | VARCHAR(50)        |                                     |
+|                      | adresse_1                                | VARCHAR(50)        |                                     |
+|                      | adresse_2_                               | VARCHAR(50)        |                                     |
+|                      | adresse_3_                               | VARCHAR(50)        |                                     |
+|                      | Telephone                                | VARCHAR(50)        |                                     |
+
+| Entraineur           | Numero_d_entraineur__identifiant_        | Auto-incrément INT | PRIMARY KEY                          |
+|                      | Nom                                      | VARCHAR(50)        |                                     |
+|                      | Prenom                                   | VARCHAR(50)        |                                     |
+|                      | Adresse_1                                | VARCHAR(50)        |                                     |
+|                      | Adresse_2                                | VARCHAR(50)        |                                     |
+|                      | Adresse_3_                               | VARCHAR(50)        |                                     |
+|                      | Telephone                                | VARCHAR(50)        |                                     |
+
+| Veterinaire          | Numero_de_veterinaire__Idantifiant_      | Auto-incrément INT | PRIMARY KEY                          |
+|                      | Nom                                      | VARCHAR(50)        |                                     |
+|                      | Prenom                                   | VARCHAR(50)        |                                     |
+|                      | Telephone                                | VARCHAR(50)        |                                     |
+|                      | Adresse_1                                | VARCHAR(50)        |                                     |
+|                      | Adresse_2                                | VARCHAR(50)        |                                     |
+|                      | Adresse_3                                | VARCHAR(50)        |                                     |
+
+| Possede              | Numero_d_enregistrement__identifiant_    | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Cheval(Numero_d_enregistrement__identifiant_) |
+|                      | Numéro_de_proprietaire__identifiant_     | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Proprietaire(Numéro_de_proprietaire__identifiant_) |
+
+| Entraine             | Numero_d_enregistrement__identifiant_    | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Cheval(Numero_d_enregistrement__identifiant_) |
+|                      | Numero_d_entraineur__identifiant_        | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Entraineur(Numero_d_entraineur__identifiant_) |
+
+| Suit                 | Numero_d_enregistrement__identifiant_    | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Cheval(Numero_d_enregistrement__identifiant_) |
+|                      | Numero_de_veterinaire__Idantifiant_      | INT                | PRIMARY KEY, FOREIGN KEY REFERENCES Veterinaire(Numero_de_veterinaire__Idantifiant_) |
