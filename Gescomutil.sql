feb394c2dc794b12fa9d1df538673e7197b9c3ce
clemence95 <boitelleclemence@outlook.fr>
2024-05-02 09:18:06 +0200

    .

--- Gescomutil.sql
+++ Gescomutil.sql
@@ -9,3 +9,12 @@ GRANT SELECT ON Gescom.* TO 'util2'@'localhost';
 -- Créer l'utilisateur util3 sans aucun privilège, sauf pour la table Customers
 CREATE USER 'util3'@'localhost' IDENTIFIED BY 'mot_de_passe';
 GRANT SELECT ON Gescom.Customers TO 'util3'@'localhost';
+
+-- Suppression de l'utilisateur 'util1' s'il existe
+DROP USER IF EXISTS 'util1'@'localhost';
+
+-- Suppression de l'utilisateur 'util2' s'il existe
+DROP USER IF EXISTS 'util2'@'localhost';
+
+-- Suppression de l'utilisateur 'util3' s'il existe
+DROP USER IF EXISTS 'util3'@'localhost';
