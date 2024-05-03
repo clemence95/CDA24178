eaa1eb9ba82323a6a6a2f341492f1ef45d37b610
clemence95 <boitelleclemence@outlook.fr>
2024-05-03 10:42:17 +0200

    .

--- BaseExemple2.sql
+++ BaseExemple2.sql
@@ -1,2 +1,7 @@
 -- Sélectionner la base de données exemple pour y effectuer les opérations suivantes
 use exemple;
+-- Recherche le prénom des employés et le numéro de la région de leur département
+SELECT e.prenom, d.noregion
+FROM employe e
+JOIN dept d ON e.nodep = d.nodept;
+
