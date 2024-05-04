a1a1b84fe597fc489f6e35ee6d4514302a1defd1
boitelle clémence <boitelleclemence@outlook.fr>
2024-05-04 17:27:27 +0200

    .

--- Lot1.sql
+++ Lot1.sql
@@ -4,6 +4,7 @@ use hotel;
 SELECT hot_nom, hot_ville
 FROM hotel;
 -- Afficher la ville de résidence de Mr White avec son nom, prénom et adresse :
-SELECT cli_nom, cli_prenom, cli_adresse
+SELECT cli_nom, cli_prenom, cli_adresse, cli_ville
 FROM client
 WHERE cli_nom = 'White';
+
