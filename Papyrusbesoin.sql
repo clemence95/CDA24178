a39ebaad1340ffc59171b4304f6b4e7a02e8ffc3
clemence95 <boitelleclemence@outlook.fr>
2024-05-06 10:03:20 +0200

    .

--- Papyrusbesoin.sql
+++ Papyrusbesoin.sql
@@ -7,3 +7,10 @@ WHERE numfou = 9120;
 SELECT DISTINCT fourn.numfou, fourn.nomfou
 FROM entcom
 JOIN fournis fourn ON entcom.numfou = fourn.numfou;
+-- Affiche le nombre de commandes fournisseurs passées, et le nombre
+-- de fournisseur concernés
+SELECT 
+    COUNT(DISTINCT numcom) AS nb_commandes,
+    COUNT(DISTINCT numfou) AS nb_fournisseurs
+FROM entcom;
+
