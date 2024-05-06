77e5b79fa1b00d60d4979ffda34b4f93f91d5906
clemence95 <boitelleclemence@outlook.fr>
2024-05-06 10:09:29 +0200

    .

--- Papyrusbesoin.sql
+++ Papyrusbesoin.sql
@@ -13,4 +13,16 @@ SELECT
     COUNT(DISTINCT numcom) AS nb_commandes,
     COUNT(DISTINCT numfou) AS nb_fournisseurs
 FROM entcom;
+-- Editer les produits ayant un stock inférieur ou égal au stock d'alerte et
+-- dont la quantité annuelle est inférieur est inférieure à 1000
+-- (informations à fournir : n° produit, libellé produit, stock, stock actuel
+-- d'alerte, quantité annuelle)
+SELECT 
+    codart AS 'N° produit',
+    libart AS 'Libellé produit',
+    stkphy AS 'Stock',
+    stkale AS 'Stock d\'alerte',
+    qteann AS 'Quantité annuelle'
+FROM produit
+WHERE stkphy <= stkale AND qteann < 1000;
 
