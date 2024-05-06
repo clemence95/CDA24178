6bdfae0a411357b24c0c278406a7cc1bdf402559
clemence95 <boitelleclemence@outlook.fr>
2024-05-06 09:53:51 +0200

    .

--- Papyrusbesoin.sql
+++ Papyrusbesoin.sql
@@ -3,3 +3,7 @@ USE papyrus;
 SELECT *
 FROM entcom
 WHERE numfou = 9120;
+-- Affiche le code des fournisseurs pour lesquels des commandes ont été passées
+SELECT DISTINCT fourn.numfou, fourn.nomfou
+FROM entcom
+JOIN fourn ON entcom.numfou = fourn.numfou;
