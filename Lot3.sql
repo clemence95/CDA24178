1ddbd570d576ce31eac67ee4e6d272c30263cf6a
clemence95 <boitelleclemence@outlook.fr>
2024-05-06 09:31:26 +0200

    .

--- Lot3.sql
+++ Lot3.sql
@@ -1,2 +1,7 @@
 -- Sélectionner la base de données exemple pour y effectuer les opérations suivantes
 use hotel;
+--  Compte le nombre d’hôtel par station
+SELECT s.sta_nom AS nom_station, COUNT(*) AS nombre_hotels
+FROM hotel h
+JOIN station s ON h.hot_sta_id = s.sta_id
+GROUP BY s.sta_nom;
