d858f02a0dae22a659bd7ed1bb579cbd62481469
clemence95 <boitelleclemence@outlook.fr>
2024-05-06 09:32:13 +0200

    .

--- Lot3.sql
+++ Lot3.sql
@@ -5,3 +5,9 @@ SELECT s.sta_nom AS nom_station, COUNT(*) AS nombre_hotels
 FROM hotel h
 JOIN station s ON h.hot_sta_id = s.sta_id
 GROUP BY s.sta_nom;
+-- Compte le nombre de chambre par station
+SELECT s.sta_nom AS nom_station, COUNT(*) AS nombre_chambres
+FROM chambre c
+JOIN hotel h ON c.cha_hot_id = h.hot_id
+JOIN station s ON h.hot_sta_id = s.sta_id
+GROUP BY s.sta_nom;
