6ff4fec607e7fd1842791e7af9e7b0c8dc2b8972
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 08:51:27 +0200

    .

--- facultatif.sql
+++ facultatif.sql
@@ -6,3 +6,11 @@ SELECT titre FROM REPRESENTATION WHERE lieu = 'Opéra Bastille';
 -- Exercice d'application n°2
 -- Pour obtenir le nombre total d'étudiants :
 SELECT COUNT(*) AS Nombre_Total_Etudiants FROM ETUDIANT;
+-- Pour trouver la note la plus haute et la note la plus basse parmi l'ensemble des notes :
+SELECT MAX(Note) AS Note_Maximale, MIN(Note) AS Note_Minimale FROM EVALUER;
+-- Pour calculer la moyenne de chaque étudiant dans chacune des matières :
+SELECT ETUDIANT.id, ETUDIANT.Nom, ETUDIANT.Prenom, MATIERE.Libellé, AVG(EVALUER.Note) AS Moyenne
+FROM ETUDIANT
+INNER JOIN EVALUER ON ETUDIANT.id = EVALUER.id_Etudiant
+INNER JOIN MATIERE ON EVALUER.id_Matiere = MATIERE.id
+GROUP BY ETUDIANT.id, ETUDIANT.Nom, ETUDIANT.Prenom, MATIERE.Libellé;
