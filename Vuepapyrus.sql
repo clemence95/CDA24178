3f2fbc46ed7d9de5130b0d1086006378d3ce369d
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 10:47:08 +0200

    .

--- Vuepapyrus.sql
+++ Vuepapyrus.sql
@@ -10,3 +10,17 @@ GROUP BY codart;
 SELECT * FROM v_GlobalCde;
 
 -- Créer la vue v_VentesI100 correspondant à la requête :
+CREATE VIEW v_VentesI100 AS
+SELECT *
+FROM vente
+WHERE codart = 'I100';
+
+SELECT * FROM v_VentesI100
+
+-- Créer la vue v_VentesI100Grobrigan à partir de la vue précédente :
+CREATE VIEW v_VentesI100Grobrigan AS
+SELECT *
+FROM v_VentesI100
+WHERE numfou = 120;
+
+SELECT * FROM v_VentesI100Grobrigan
