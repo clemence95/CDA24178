c1cd7d0da294bfc7a914a3b59100d6082cb2c784
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 11:34:53 +0200

    .

--- Filrouge/Filrouge_rtdb.sql
+++ Filrouge/Filrouge_rtdb.sql
@@ -1,7 +1,9 @@
 USE VillageGreen;
 -- Chiffre d'affaires mois par mois pour une année sélectionnée :
-SELECT MONTH(Date_facturation) AS Mois, SUM(Total_TTC) AS CA_Mois
+SELECT MONTH(Date_facturation) AS Mois, YEAR(Date_facturation) AS Annee, SUM(Total_TTC) AS Chiffre_affaires
 FROM Commande
-WHERE YEAR(Date_facturation) = 2024
+WHERE YEAR(Date_facturation) = '2024'
 GROUP BY Mois;
+
+
 
