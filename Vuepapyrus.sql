d405c1247b9ce80cd13c643ec9744ff84f0c8367
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 10:42:30 +0200

    .

--- Vuepapyrus.sql
+++ Vuepapyrus.sql
@@ -1,2 +1,12 @@
 use papyrus;
 -- Créer la vue v_GlobalCde correspondant à la requête :
+CREATE VIEW v_GlobalCde AS
+SELECT codart, 
+       SUM(qtecde) AS QteTot, 
+       SUM(qtecde * priuni) AS PrixTot
+FROM ligcom
+GROUP BY codart;
+
+SELECT * FROM v_GlobalCde;
+
+-- Créer la vue v_VentesI100 correspondant à la requête :
