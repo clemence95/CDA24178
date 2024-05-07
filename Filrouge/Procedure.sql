6a3acec9185a0bf0e5d6cdcae060925b6ef89449
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 14:46:34 +0200

    .

---
+++ Filrouge/Procedure.sql
@@ -0,0 +1,12 @@
+USE VillageGreen;
+-- Sélection des commandes non soldées (en cours de livraison) :
+DELIMITER //
+
+CREATE PROCEDURE SelectCommandesEnCoursDeLivraison()
+BEGIN
+    SELECT * FROM Commande WHERE Statut = 'En cours de livraison';
+END//
+
+DELIMITER ;
+
+CALL SelectCommandesEnCoursDeLivraison();
