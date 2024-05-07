80a5e9bd8c1987c5221052d7a2b1dbb880a8367d
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 08:40:06 +0200

    .

--- facultatif.sql
+++ facultatif.sql
@@ -4,3 +4,5 @@ SELECT titre FROM REPRESENTATION;
 -- Pour obtenir la liste des titres des représentations ayant lieu à l'opéra Bastille, nous utilisons la requête suivante :
 SELECT titre FROM REPRESENTATION WHERE lieu = 'Opéra Bastille';
 -- Exercice d'application n°2
+-- Pour obtenir le nombre total d'étudiants :
+SELECT COUNT(*) AS Nombre_Total_Etudiants FROM ETUDIANT;
