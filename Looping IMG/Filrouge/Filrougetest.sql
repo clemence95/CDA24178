0170adf62e6cdde7fd73d86e618f69545bcf28f0
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 11:12:45 +0200

    .

---
+++ Looping IMG/Filrouge/Filrougetest.sql
@@ -0,0 +1,124 @@
+USE VillageGreen;
+-- Insertion des données dans la table Fournisseur
+INSERT INTO Fournisseur (Id_Fournisseur, Nom, Contact, telephone)
+VALUES 
+    (1, 'Guitar World', 'Michael Johnson', '0123456789'),
+    (2, 'Piano Universe', 'Sarah Lee', '0987654321'),
+    (3, 'Music Emporium', 'David Wilson', '0123456789'),
+    (4, 'Melody Instruments', 'Jessica Brown', '0987654321'),
+    (5, 'Rhythm Warehouse', 'Michael Clark', '0123456789'),
+    (6, 'Harmony Supplies', 'Emma Turner', '0987654321'),
+    (7, 'Beat Studio', 'James Lee', '0123456789'),
+    (8, 'Note Haven', 'Sophia Martinez', '0987654321'),
+    (9, 'Song Mart', 'Daniel Thompson', '0123456789'),
+    (10, 'Sound Waves', 'Olivia Harris', '0987654321');
+
+-- Insertion des données dans la table Client
+INSERT INTO Client (Id_Client, Nom, prenom, telephone, Type, Adresse_livraison, Adresse_facturation, Coefficient, Reduction, Reference, email)
+VALUES 
+    (1, 'MusicAddict', 'Thomas', '0234567890', 'Particulier', '25 rue des Mélodies', '25 rue des Factures', 1.0, NULL, 'MA001', 'thomas@musicaddict.com'),
+    (2, 'Symphony Corp.', 'Sophie', '0678901234', 'Entreprise', '10 avenue des Harmonies', '10 avenue des Factures', 1.5, 0.2, 'SC001', 'sophie@symphony.com'),
+    (3, 'MusicEnthusiast', 'Christopher', '0234567890', 'Particulier', '40 rue des Mélodies', '40 rue des Factures', 1.0, NULL, 'ME001', 'christopher@musicenthusiast.com'),
+    (4, 'Harmonics Ltd.', 'Emma', '0678901234', 'Entreprise', '20 avenue des Harmonies', '20 avenue des Factures', 1.5, 0.2, 'HL001', 'emma@harmonicsltd.com'),
+    (5, 'Songbird Inc.', 'Daniel', '0123456789', 'Particulier', '5 rue des Chansons', '5 rue des Factures', 1.0, NULL, 'SB001', 'daniel@songbirdinc.com'),
+    (6, 'MusicAddict', 'Thomas', '0234567890', 'Particulier', '25 rue des Mélodies', '25 rue des Factures', 1.0, NULL, 'MA001', 'thomas@musicaddict.com'),
+    (7, 'Symphony Corp.', 'Sophie', '0678901234', 'Entreprise', '10 avenue des Harmonies', '10 avenue des Factures', 1.5, 0.2, 'SC001', 'sophie@symphony.com'),
+    (8, 'MusicEnthusiast', 'Christopher', '0234567890', 'Particulier', '40 rue des Mélodies', '40 rue des Factures', 1.0, NULL, 'ME001', 'christopher@musicenthusiast.com'),
+    (9, 'Harmonics Ltd.', 'Emma', '0678901234', 'Entreprise', '20 avenue des Harmonies', '20 avenue des Factures', 1.5, 0.2, 'HL001', 'emma@harmonicsltd.com'),
+    (10, 'Songbird Inc.', 'Daniel', '0123456789', 'Particulier', '5 rue des Chansons', '5 rue des Factures', 1.0, NULL, 'SB001', 'daniel@songbirdinc.com');
+
+-- Insertion des données dans la table Commercial
+INSERT INTO Commercial (Id_Commercial, Nom, prenom, telephone, email, Id_Client)
+VALUES 
+    (1, 'Alex Parker', 'Alex', '0456789012', 'alex@guitarworld.com', 1),
+    (2, 'Emma White', 'Emma', '0890123456', 'emma@pianouniverse.com', 2),
+    (3, 'Alex Parker', 'Alex', '0456789012', 'alex@guitarworld.com', 3),
+    (4, 'Emma White', 'Emma', '0890123456', 'emma@pianouniverse.com', 4),
+    (5, 'Alex Parker', 'Alex', '0456789012', 'alex@guitarworld.com', 5),
+    (6, 'Emma White', 'Emma', '0890123456', 'emma@pianouniverse.com', 6),
+    (7, 'Alex Parker', 'Alex', '0456789012', 'alex@guitarworld.com', 7),
+    (8, 'Emma White', 'Emma', '0890123456', 'emma@pianouniverse.com', 8),
+    (9, 'Alex Parker', 'Alex', '0456789012', 'alex@guitarworld.com', 9),
+    (10, 'Emma White', 'Emma', '0890123456', 'emma@pianouniverse.com', 10);
+
+-- Insertion des données dans la table Commande
+INSERT INTO Commande (Id_Commande, Statut, Mode_paiement, Reduction_pro, Total_HT, Total_TTC, Date_heure_commande, Mode_differe, Date_facturation, Id_Client)
+VALUES 
+    (1, 'En cours', 'Chèque', NULL, 1500.00, 1575.00, '2024-05-02 10:00:00', 'Différé', '2024-05-03', 1),
+    (2, 'En cours', 'Virement bancaire', 0.15, 3000.00, 3300.00, '2024-05-02 11:30:00', 'Différé', '2024-05-03', 2),
+    (3, 'En cours', 'Chèque', NULL, 1500.00, 1575.00, '2024-05-02 10:00:00', 'Différé', '2024-05-03', 3),
+    (4, 'En cours', 'Virement bancaire', 0.15, 3000.00, 3300.00, '2024-05-02 11:30:00', 'Différé', '2024-05-03', 4),
+    (5, 'En cours', 'Chèque', NULL, 1500.00, 1575.00, '2024-05-02 10:00:00', 'Différé', '2024-05-03', 5),
+    (6, 'En cours', 'Virement bancaire', 0.15, 3000.00, 3300.00, '2024-05-02 11:30:00', 'Différé', '2024-05-03', 6),
+    (7, 'En cours', 'Chèque', NULL, 1500.00, 1575.00, '2024-05-02 10:00:00', 'Différé', '2024-05-03', 7),
+    (8, 'En cours', 'Virement bancaire', 0.15, 3000.00, 3300.00, '2024-05-02 11:30:00', 'Différé', '2024-05-03', 8),
+    (9, 'En cours', 'Chèque', NULL, 1500.00, 1575.00, '2024-05-02 10:00:00', 'Différé', '2024-05-03', 9),
+    (10, 'En cours', 'Virement bancaire', 0.15, 3000.00, 3300.00, '2024-05-02 11:30:00', 'Différé', '2024-05-03', 10);
+
+-- Insertion des données dans la table BonLivraison
+INSERT INTO BonLivraison (Id_BonLivraison, Date_livraison, Statut, Suivi_commande, Id_Commande)
+VALUES 
+    (1, '2024-05-03', 'En cours de préparation', '9876543210', 1),
+    (2, '2024-05-04', 'En cours de préparation', '', 2),
+    (3, '2024-05-03', 'En cours de préparation', '9876543210', 3),
+    (4, '2024-05-04', 'En cours de préparation', '', 4),
+    (5, '2024-05-03', 'En cours de préparation', '9876543210', 5),
+    (6, '2024-05-04', 'En cours de préparation', '', 6),
+    (7, '2024-05-03', 'En cours de préparation', '9876543210', 7),
+    (8, '2024-05-04', 'En cours de préparation', '', 8),
+    (9, '2024-05-03', 'En cours de préparation', '9876543210', 9),
+    (10, '2024-05-04', 'En cours de préparation', '', 10);
+
+-- Insertion des données dans la table Categorie
+INSERT INTO Categorie (Id_Catégorie, Libelle_court, Photo)
+VALUES 
+    (1, 'Batteries', NULL),
+    (2, 'Accessoires', NULL);
+
+-- Insertion des données dans la table Souscategorie
+INSERT INTO Souscategorie (Id_Souscategorie, Libelle_court, Photo, Id_Catégorie)
+VALUES 
+    (1, 'Batteries électroniques', NULL, 1),
+    (2, 'Accordeurs', NULL, 2);
+
+-- Insertion des données dans la table Produit
+INSERT INTO Produit (Id_Produit, Libelle_court, Libelle_long, Prix_achat_HT, Photo, stock, Actif, Id_Souscategorie, Id_Fournisseur)
+VALUES 
+    (1, 'Batterie Roland', 'Batterie électronique avec pads sensibles', 1500.00, '', 15.00, 'Oui', 1, 1),
+    (2, 'Accordeur chromatique Korg', 'Accordeur polyvalent pour tous types d''instruments', 50.00, '', 50.00, 'Oui', 2, 2),
+    (3, 'Batterie Roland', 'Batterie électronique avec pads sensibles', 1500.00, '', 15.00, 'Oui', 1, 3),
+    (4, 'Accordeur chromatique Korg', 'Accordeur polyvalent pour tous types d''instruments', 50.00, '', 50.00, 'Oui', 2, 4),
+    (5, 'Batterie Roland', 'Batterie électronique avec pads sensibles', 1500.00, '', 15.00, 'Oui', 1, 5),
+    (6, 'Accordeur chromatique Korg', 'Accordeur polyvalent pour tous types d''instruments', 50.00, '', 50.00, 'Oui', 2, 6),
+    (7, 'Batterie Roland', 'Batterie électronique avec pads sensibles', 1500.00, '', 15.00, 'Oui', 1, 7),
+    (8, 'Accordeur chromatique Korg', 'Accordeur polyvalent pour tous types d''instruments', 50.00, '', 50.00, 'Oui', 2, 8),
+    (9, 'Batterie Roland', 'Batterie électronique avec pads sensibles', 1500.00, '', 15.00, 'Oui', 1, 9),
+    (10, 'Accordeur chromatique Korg', 'Accordeur polyvalent pour tous types d''instruments', 50.00, '', 50.00, 'Oui', 2, 10);
+
+-- Insertion des données dans la table achete
+INSERT INTO achete (Id_Produit, Id_Commande, quantite)
+VALUES 
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 3, 1),
+    (4, 4, 2),
+    (5, 5, 1),
+    (6, 6, 2),
+    (7, 7, 1),
+    (8, 8, 2),
+    (9, 9, 1),
+    (10, 10, 2);
+
+-- Insertion des données dans la table livre
+INSERT INTO livre (Id_Produit, Id_BonLivraison, quantite)
+VALUES 
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 3, 1),
+    (4, 4, 2),
+    (5, 5, 1),
+    (6, 6, 2),
+    (7, 7, 1),
+    (8, 8, 2),
+    (9, 9, 1),
+    (10, 10, 2);
