931e12ce65e454aa417a5b94705506ae1c1a23e9
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 12:28:12 +0200

    .

--- Filrouge/Filrouge_rtdb.sql
+++ Filrouge/Filrouge_rtdb.sql
@@ -4,6 +4,18 @@ SELECT MONTH(Date_facturation) AS Mois, YEAR(Date_facturation) AS Annee, SUM(Tot
 FROM Commande
 WHERE YEAR(Date_facturation) = '2024'
 GROUP BY Mois;
+-- Chiffre d'affaires Fournisseur
+SELECT Fournisseur.Nom AS Nom_Fournisseur,
+       SUM(Commande.Total_TTC) AS Chiffre_affaires
+FROM Commande
+INNER JOIN achete ON Commande.Id_Commande = achete.Id_Commande
+INNER JOIN Produit ON achete.Id_Produit = Produit.Id_Produit
+INNER JOIN Fournisseur ON Produit.Id_Fournisseur = Fournisseur.Id_Fournisseur
+GROUP BY Fournisseur.Nom;
+
+
+
+
 
 
 
