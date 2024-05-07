d86c32261e5201205aa8d219575ca7386f05c48c
clemence95 <boitelleclemence@outlook.fr>
2024-05-07 08:39:06 +0200

    .

--- facultatif.sql
+++ facultatif.sql
@@ -1,4 +1,6 @@
+-- Exercice d'application n°1
 -- Pour obtenir la liste des titres des représentations, nous utilisons la requête suivante :
 SELECT titre FROM REPRESENTATION;
 -- Pour obtenir la liste des titres des représentations ayant lieu à l'opéra Bastille, nous utilisons la requête suivante :
 SELECT titre FROM REPRESENTATION WHERE lieu = 'Opéra Bastille';
+-- Exercice d'application n°2
