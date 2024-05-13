b4efccdd1a4b9056aac7b76293710cbaddf4b4d5
clemence95 <boitelleclemence@outlook.fr>
2024-05-13 11:07:05 +0200

    .

--- Papyrus_procedures.sql
+++ Papyrus_procedures.sql
@@ -5,27 +5,44 @@ DELIMITER //
 
 CREATE PROCEDURE Lst_fournis()
 BEGIN
-    SELECT DISTINCT numfou
-    FROM entcom;
+    SELECT DISTINCT fourn.numfou, fourn.nomfou
+    FROM entcom
+    JOIN fournis fourn ON entcom.numfou = fourn.numfou;
 END //
 
 DELIMITER ;
+
 
 SHOW CREATE PROCEDURE Lst_fournis;
 
 --  procédure stockée Lst_Commandes : 
-
 DELIMITER //
 
-CREATE PROCEDURE Lst_Commandes (IN obscom_filter VARCHAR(50))
+CREATE PROCEDURE Lst_Commandes(IN obscom_param VARCHAR(50))
 BEGIN
-    -- Instructions SQL pour sélectionner les commandes ayant le libellé particulier
-    SELECT *
+    SELECT entcom.numcom AS 'Numéro de commande',
+           fourn.nomfou AS 'Nom du fournisseur',
+           produit.libart AS 'Libellé du produit',
+           ligcom.qteliv AS 'Quantité commandée',
+           ligcom.priuni AS 'Prix unitaire',
+           (ligcom.qteliv * ligcom.priuni) AS 'Total du produit',
+           total_commande.total AS 'Total de la commande'
     FROM entcom
-    WHERE obscom = obscom_filter;
+    JOIN fournis AS fourn ON entcom.numfou = fourn.numfou
+    JOIN ligcom ON entcom.numcom = ligcom.numcom
+    JOIN produit ON ligcom.codart = produit.codart
+    JOIN (
+        SELECT numcom, SUM(qteliv * priuni) AS total
+        FROM ligcom
+        GROUP BY numcom
+    ) AS total_commande ON entcom.numcom = total_commande.numcom
+    WHERE entcom.obscom LIKE CONCAT('%', obscom_param, '%')
+    ORDER BY entcom.numcom, fourn.nomfou;
 END //
 
 DELIMITER ;
 
+
 SHOW CREATE PROCEDURE Lst_Commandes;
 
+DROP PROCEDURE `Lst_Commandes`;
