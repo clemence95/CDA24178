e64d14bd5a411f2d37edcea1fa96eee7267667da
clemence95 <boitelleclemence@outlook.fr>
2024-05-14 09:07:03 +0200

    .

--- Hotel_declencheurs.sql
+++ Hotel_declencheurs.sql
@@ -29,3 +29,28 @@ DELIMITER ;
 UPDATE reservation
 SET res_date_fin = '2024-05-21'
 WHERE res_id = 1;
+
+USE hotel;
+
+DELIMITER //
+
+CREATE TRIGGER insert_reservation BEFORE INSERT ON reservation
+FOR EACH ROW
+BEGIN
+    DECLARE num_reservations INT;
+    DECLARE max_reservations INT;
+    
+    -- Obtention du nombre de réservations actuelles pour l'hôtel concerné
+    SELECT COUNT(*) INTO num_reservations FROM reservation WHERE res_cha_id = NEW.res_cha_id;
+    
+    -- Obtention du nombre maximal de réservations autorisées (dans ce cas, 10)
+    SELECT 10 INTO max_reservations;
+    
+    -- Vérification si le nombre de réservations actuelles dépasse le maximum autorisé
+    IF num_reservations >= max_reservations THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Impossible d''ajouter une réservation, l''hôtel a déjà atteint le nombre maximal de réservations.';
+    END IF;
+END//
+
+DELIMITER ;
+
