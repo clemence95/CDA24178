1cdc0d15ccb9a750fe1978940452a5ff386f337d
clemence95 <boitelleclemence@outlook.fr>
2024-05-14 09:19:32 +0200

    .

--- Hotel_declencheurs.sql
+++ Hotel_declencheurs.sql
@@ -55,4 +55,27 @@ END//
 DELIMITER ;
 
 INSERT INTO reservation (res_cha_id, res_cli_id, res_date, res_date_debut, res_date_fin, res_prix, res_arrhes) 
-VALUES (1, 1, '2024-05-15', '2024-06-01', '2024-06-15', 2000, 500);
+VALUES (2, 1, '2024-05-15', '2024-06-01', '2024-06-15', 2000, 500);
+
+DELIMITER //
+
+CREATE TRIGGER insert_reservation2
+BEFORE INSERT ON reservation
+FOR EACH ROW
+BEGIN
+    DECLARE num_reservations INT;
+    
+    -- Obtenir le nombre de réservations actuelles pour le client concerné
+    SELECT COUNT(*) INTO num_reservations
+    FROM reservation
+    WHERE res_cli_id = NEW.res_cli_id;
+    
+    -- Vérifier si le client a déjà 3 réservations
+    IF num_reservations >= 3 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Impossible d\'ajouter une réservation, le client a déjà atteint le nombre maximal de réservations.';
+    END IF;
+END;
+//
+
+DELIMITER ;
