674c239014f5d6e1bcf6c0dc7a4fbfe5f483fd51
clemence95 <boitelleclemence@outlook.fr>
2024-05-14 10:10:19 +0200

    .

--- Hotel_declencheurs.sql
+++ Hotel_declencheurs.sql
@@ -59,23 +59,25 @@ VALUES (2, 1, '2024-05-15', '2024-06-01', '2024-06-15', 2000, 500);
 
 DELIMITER //
 
-CREATE TRIGGER insert_reservation2
-BEFORE INSERT ON reservation
-FOR EACH ROW
-BEGIN
-    DECLARE num_reservations INT;
+CREATE TRIGGER insert_reservation2 -- CREATE TRIGGER insert_reservation2 : C'est la déclaration pour créer un nouveau déclencheur nommé insert_reservation2.
+BEFORE INSERT ON reservation -- BEFORE INSERT ON reservation : Indique que le déclencheur doit être déclenché avant l'insertion d'une nouvelle ligne dans la table reservation.
+FOR EACH ROW -- FOR EACH ROW : Spécifie que le déclencheur s'exécute pour chaque ligne qui est sur le point d'être insérée dans la table.
+BEGIN -- BEGIN : Marque le début du bloc de code exécuté par le déclencheur.
+    DECLARE num_reservations INT; -- DECLARE num_reservations INT; : Déclare une variable num_reservations de type entier qui sera utilisée pour stocker le nombre de réservations du client concerné.
+
     
     -- Obtenir le nombre de réservations actuelles pour le client concerné
     SELECT COUNT(*) INTO num_reservations
-    FROM reservation
+    FROM reservation -- Cette requête SQL compte le nombre de réservations
+    -- pour le client de la nouvelle réservation (NEW.res_cli_id) et stocke le résultat dans la variable num_reservations.
     WHERE res_cli_id = NEW.res_cli_id;
     
     -- Vérifier si le client a déjà 3 réservations
-    IF num_reservations >= 3 THEN
+    IF num_reservations >= 3 THEN -- Vérifie si le nombre de réservations du client est supérieur ou égal à 3.
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Impossible d\'ajouter une réservation, le client a déjà atteint le nombre maximal de réservations.';
-    END IF;
+    END IF;   -- Si le client a déjà 3 réservations ou plus, cette instruction génère une erreur avec le message spécifié, ce qui annule l'insertion de la nouvelle réservation.
 END;
 //
 
-DELIMITER ;
+DELIMITER ; -- : Marque la fin du bloc de code du déclencheur.
