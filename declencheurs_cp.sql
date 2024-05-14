eb140df8fa86a05ef1c0a288b5754cec2986819f
clemence95 <boitelleclemence@outlook.fr>
2024-05-14 13:17:49 +0200

    .

--- declencheurs_cp.sql
+++ declencheurs_cp.sql
@@ -2,7 +2,23 @@ USE cp;
 
 ALTER TABLE client ENGINE=InnoDB;
 
+DELIMITER |
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcule le total
+        -- SET tot = ??? (prévoir le calcul de la remise) 
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+    END;
+|
+DELIMITER ;
 
+INSERT INTO lignedecommande (id_commande, id_produit, prix, quantite) VALUES (3, 4, 100, 3);
+
+SELECT * FROM commande WHERE id = 3;
 
 
 
