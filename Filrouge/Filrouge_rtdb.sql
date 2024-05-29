995feb11b9a894ae6564ff57ff21061d3f32a717
clemence95 <boitelleclemence@outlook.fr>
2024-05-29 13:06:03 +0200

    .

--- Filrouge/Filrouge_rtdb.sql
+++ Filrouge/Filrouge_rtdb.sql
@@ -1,34 +1,36 @@
--- Chiffre d'affaires mois par mois pour une année sélectionnée et pour tous les fournisseurs :
-SELECT
-    MONTH(Date_facturation) AS Mois,
-    YEAR(Date_facturation) AS Annee,
-    'VillageGreen' AS Nom_Fournisseur, -- Ajout d'une colonne vide pour aligner avec la deuxième requête
-    SUM(Total_TTC) AS Chiffre_affaires
-FROM Commande
-WHERE
-    YEAR(Date_facturation) = '2024'
-GROUP BY
-    Annee,
-    Mois
-
+-- Chiffre d'affaires mois par mois pour une année sélectionnée et pour tous les fournisseurs :(
+    (
+    SELECT
+        MONTH(Date_facturation) AS Mois,
+        YEAR(Date_facturation) AS Annee,
+        'VillageGreen' AS Nom_Fournisseur,
+        SUM(Total_TTC) AS Chiffre_affaires
+    FROM Commande
+    WHERE
+        YEAR(Date_facturation) = 2024
+    GROUP BY
+        Annee,
+        Mois
+)
 UNION ALL
-
-SELECT
-    MONTH(Commande.Date_facturation) AS Mois,
-    YEAR(Commande.Date_facturation) AS Annee,
-    Fournisseur.Nom AS Nom_Fournisseur,
-    SUM(Commande.Total_TTC) AS Chiffre_affaires
-FROM
-    Commande
-    INNER JOIN achete ON Commande.Id_Commande = achete.Id_Commande
-    INNER JOIN Produit ON achete.Id_Produit = Produit.Id_Produit
-    INNER JOIN Fournisseur ON Produit.Id_Fournisseur = Fournisseur.Id_Fournisseur
-WHERE
-    YEAR(Commande.Date_facturation) = '2024'
-GROUP BY
-    Annee,
-    Mois,
-    Nom_Fournisseur;
+(
+    SELECT
+        MONTH(Commande.Date_facturation) AS Mois,
+        YEAR(Commande.Date_facturation) AS Annee,
+        Fournisseur.Nom AS Nom_Fournisseur,
+        SUM(Commande.Total_TTC) AS Chiffre_affaires
+    FROM
+        Commande
+        INNER JOIN achete ON Commande.Id_Commande = achete.Id_Commande
+        INNER JOIN Produit ON achete.Id_Produit = Produit.Id_Produit
+        INNER JOIN Fournisseur ON Produit.Id_Fournisseur = Fournisseur.Id_Fournisseur
+    WHERE
+        YEAR(Commande.Date_facturation) = 2024
+    GROUP BY
+        Annee,
+        Mois,
+        Nom_Fournisseur
+);
 
 
 -- TOP 10 des produits les plus commandés pour une année sélectionnée (référence et nom du produit, quantité commandée, fournisseur)
