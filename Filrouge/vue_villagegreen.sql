83f2b0cf219bf284dcd10ff331f6105a1e9572b8
clemence95 <boitelleclemence@outlook.fr>
2024-06-12 14:44:06 +0200

    .

--- Filrouge/vue_villagegreen.sql
+++ Filrouge/vue_villagegreen.sql
@@ -1,15 +1,42 @@
 USE VillageGreen;
 -- Vue correspondant à la jointure Produits - Fournisseurs :
-CREATE VIEW Vue_Produit_Fournisseur AS
-SELECT p.Id_Produit, p.Libelle_court AS Produit, p.Libelle_long AS Description, p.Prix_achat_HT AS Prix,
-       f.Nom AS Fournisseur
-FROM Produit p
-JOIN Fournisseur f ON p.Id_Fournisseur = f.Id_Fournisseur;
+CREATE VIEW Vue_Produits_Fournisseurs AS
+SELECT
+    p.Id_Produit,
+    p.Libelle_court AS Produit,
+    p.Libelle_long AS Description,
+    p.Prix_achat_HT AS Prix_achat_HT,
+    p.Photo AS Photo_produit,
+    p.stock AS Stock,
+    p.Actif AS Actif,
+    f.Id_Fournisseur,
+    f.Nom AS Fournisseur,
+    f.Contact AS Contact_fournisseur,
+    f.telephone AS Telephone_fournisseur
+FROM
+    produit p
+JOIN
+    Fournisseur f ON p.Id_Fournisseur = f.Id_Fournisseur;
+
 
 -- Vue correspondant à la jointure Produits - Catégorie/Sous-catégorie :
-CREATE VIEW Vue_Produit_Categorie AS
-SELECT p.Id_Produit, p.Libelle_court AS Produit, p.Libelle_long AS Description, p.Prix_achat_HT AS Prix,
-       c.Libelle_court AS Categorie, sc.Libelle_court AS Souscategorie
-FROM Produit p
-JOIN Souscategorie sc ON p.Id_Souscategorie = sc.Id_Souscategorie
-JOIN Categorie c ON sc.Id_Catégorie = c.Id_Catégorie;
+CREATE VIEW Vue_Produits_Categories AS
+SELECT
+    p.Id_Produit,
+    p.Libelle_court AS Produit,
+    p.Libelle_long AS Description,
+    p.Prix_achat_HT AS Prix_achat_HT,
+    p.Photo AS Photo_produit,
+    p.stock AS Stock,
+    p.Actif AS Actif,
+    sc.Id_Souscategorie,
+    sc.Libelle_court AS Souscategorie,
+    c.Id_Categorie,
+    c.Libelle_court AS Categorie
+FROM
+    produit p
+JOIN
+    souscategorie sc ON p.Id_Souscategorie = sc.Id_Souscategorie
+JOIN
+    categorie c ON sc.Id_Categorie = c.Id_Categorie;
+
